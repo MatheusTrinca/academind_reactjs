8bb4f1ff2f97a2030df23c0e45f72e8e55d892ac
Matheus Trinca <matheusfelipetrinca@gmail.com>
2021-07-30 20:53:11 -0300

    meals

--- 08-Food-Order-App/src/App.js
+++ 08-Food-Order-App/src/App.js
@@ -1,9 +1,13 @@
 const { default: Header } = require('./components/Layout/Header');
+const { default: Meals } = require('./components/Meals/Meals');
 
 function App() {
   return (
     <>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </>
   );
 }
